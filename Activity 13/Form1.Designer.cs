9e51f860cee62049b1a95450edf2d86d52dd9b98
Cory-Watt <106133667+Cory-Watt@users.noreply.github.com>
2022-06-30 00:43:55 -0400

    Add files via upload

---
+++ Activity 13/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Activity_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.c3 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a1 = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.restart = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.c3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.c2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.c1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.b3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.b1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.a3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.a2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.a1, 0, 0);
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(334, 296);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // c3
+            // 
+            this.c3.Location = new System.Drawing.Point(226, 200);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(102, 90);
+            this.c3.TabIndex = 8;
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // c2
+            // 
+            this.c2.Location = new System.Drawing.Point(116, 200);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(101, 90);
+            this.c2.TabIndex = 7;
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.c2_Click);
+            // 
+            // c1
+            // 
+            this.c1.Location = new System.Drawing.Point(6, 200);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(101, 90);
+            this.c1.TabIndex = 6;
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.c1_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(226, 103);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(102, 88);
+            this.b3.TabIndex = 5;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(116, 103);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(101, 88);
+            this.b2.TabIndex = 4;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(6, 103);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(101, 88);
+            this.b1.TabIndex = 3;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // a3
+            // 
+            this.a3.Location = new System.Drawing.Point(226, 6);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(102, 88);
+            this.a3.TabIndex = 2;
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.a3_Click);
+            // 
+            // a2
+            // 
+            this.a2.Location = new System.Drawing.Point(116, 6);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(101, 88);
+            this.a2.TabIndex = 1;
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.a2_Click);
+            // 
+            // a1
+            // 
+            this.a1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(6, 6);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(101, 88);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.Click += new System.EventHandler(this.a1_Click);
+            // 
+            // exit
+            // 
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exit.Location = new System.Drawing.Point(196, 314);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 1;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // restart
+            // 
+            this.restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restart.Location = new System.Drawing.Point(88, 314);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(75, 23);
+            this.restart.TabIndex = 2;
+            this.restart.Text = "Restart";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(357, 343);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe - Cory Wattenberger";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button restart;
+    }
+}
+
---
+++ Activity 13/Form1.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_13
+{
+    public partial class Form1 : Form
+    {
+        //initialize variables
+        public int whosTurn;
+        public int[,] grid = new int[3, 3];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //set player to 1 turn when the form loads
+            whosTurn = 1;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Exit program when the exit button is clicked
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //reset values to null when the reset button is clicked
+        private void restart_Click(object sender, EventArgs e)
+        {
+            //blank out values
+            a1.Text = null;
+            a2.Text = null;
+            a3.Text = null;
+            b1.Text = null;
+            b2.Text = null;
+            b3.Text = null;
+            c1.Text = null;
+            c2.Text = null;
+            c3.Text = null;
+
+            //re-enable buttons
+            a1.Enabled = true;
+            a2.Enabled = true;
+            a3.Enabled = true;
+            b1.Enabled = true;
+            b2.Enabled = true;
+            b3.Enabled = true;
+            c1.Enabled = true;
+            c2.Enabled = true;
+            c3.Enabled = true;
+
+            //reset grid values to 0
+            for (int i = 0; i < 3; i++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    grid[i, x] = 0;
+                }
+            }
+        }
+
+        private void a1_Click(object sender, EventArgs e)
+        {
+
+            //disable button after it has been selected
+            a1.Enabled = false;
+
+            //update grid with which player selected which button
+            if (whosTurn == 1)
+            {
+                grid[0, 0] = 1;
+                a1.Text = "X";
+                whosTurn = 2;
+            }
+            else
+            {
+                grid[0, 0] = 2;
+                a1.Text = "O";
+                whosTurn = 1;
+            }
+            //run functions to check for a winner 
+            CheckRows();
+            CheckColumns();
+            CheckDiagonals();
+        }
+
+        private void a2_Click(object sender, EventArgs e)
+        {
+            //disable button after it has been selected
+            a2.Enabled = false;
+
+            //update grid with which player selected which button
+            if (whosTurn == 1)
+            {
+                grid[0, 1] = 1;
+                a2.Text = "X";
+                whosTurn = 2;
+            }
+            else
+            {
+                grid[0, 1] = 2;
+                a2.Text = "O";
+                whosTurn = 1;
+            }
+
+            //run functions to check for a winner
+            CheckRows();
+            CheckColumns();
+            CheckDiagonals();
+        }
+
+        private void a3_Click(object sender, EventArgs e)
+        {
+            //disable button after it has been selected
+            a3.Enabled = false;
+
+            //update grid with which player selected which button
+            if (whosTurn == 1)
+            {
+                grid[0, 2] = 1;
+                a3.Text = "X";
+                whosTurn = 2;
+            }
+            else
+            {
+                grid[0, 2] = 2;
+                a3.Text = "O";
+                whosTurn = 1;
+            }
+            //run functions to check for a winner
+            CheckRows();
+            CheckColumns();
+            CheckDiagonals();
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            //disable button after it has been selected
+            b1.Enabled = false;
+
+            //update grid with which player selected which button
+            if (whosTurn == 1)
+            {
+                grid[1, 0] = 1;
+                b1.Text = "X";
+                whosTurn = 2;
+            }
+            else
+            {
+                grid[1, 0] = 2;
+                b1.Text = "O";
+                whosTurn = 1;
+            }
+            //run functions to check for a winner
+            CheckRows();
+            CheckColumns();
+            CheckDiagonals();
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            //disable button after it has been selected
+            b2.Enabled = false;
+
+            //update grid with which player selected which button
+            if (whosTurn == 1)
+            {
+                grid[1, 1] = 1;
+                b2.Text = "X";
+                whosTurn = 2;
+            }
+            else
+            {
+                grid[1, 1] = 2;
+                b2.Text = "O";
+                whosTurn = 1;
+            }
+            //run functions to check for a winner
+            CheckRows();
+            CheckColumns();
+            CheckDiagonals();
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            //disable button after it has been selected
+            b3.Enabled = false;
+
+            //update grid with which player selected which button
+            if (whosTurn == 1)
+            {
+                grid[1, 2] = 1;
+                b3.Text = "X";
+                whosTurn = 2;
+            }
+            else
+            {
+                grid[1, 2] = 2;
+                b3.Text = "O";
+                whosTurn = 1;
+            }
+            //run functions to check for a winner
+            CheckRows();
+            CheckColumns();
+            CheckDiagonals();
+        }
+
+        private void c1_Click(object sender, EventArgs e)
+        {
+            //disable button after it has been selected
+            c1.Enabled = false;
+
+            //update grid with which player selected which button
+            if (whosTurn == 1)
+            {
+                grid[2, 0] = 1;
+                c1.Text = "X";
+                whosTurn = 2;
+            }
+            else
+            {
+                grid[2, 0] = 2;
+                c1.Text = "O";
+                whosTurn = 1;
+            }
+            //run functions to check for a winner
+            CheckRows();
+            CheckColumns();
+            CheckDiagonals();
+        }
+
+        private void c2_Click(object sender, EventArgs e)
+        {
+            //disable button after it has been selected
+            c2.Enabled = false;
+
+            //update grid with which player selected which button
+            if (whosTurn == 1)
+            {
+                grid[2, 1] = 1;
+                c2.Text = "X";
+                whosTurn = 2;
+            }
+            else
+            {
+                grid[2, 1] = 2;
+                c2.Text = "O";
+                whosTurn = 1;
+            }
+            //run functions to check for a winner
+            CheckRows();
+            CheckColumns();
+            CheckDiagonals();
+        }
+
+        private void c3_Click(object sender, EventArgs e)
+        {
+            //disable button after it has been selected
+            c3.Enabled = false;
+
+            //update grid with which player selected which button     
+            if (whosTurn == 1)
+            {
+                grid[2, 2] = 1;
+                c3.Text = "X";
+                whosTurn = 2;
+            }
+            else
+            {
+                grid[2, 2] = 2;
+                c3.Text = "O";
+                whosTurn = 1;
+            }
+            //run functions to check for a winner
+            CheckRows();
+            CheckColumns();
+            CheckDiagonals();
+
+        }
+
+        //checks for 3 matches in each column
+        private void CheckColumns()
+        {
+            int P1Count = 0;
+            int P2Count = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                //reset count to 0 after each iteration of the loop
+                P1Count = 0;
+                P2Count = 0;
+
+                //check columns for 3 matches
+                for (int x = 0; x < 3; x++)
+                {
+                    if (grid[x, i] == 1)
+                    {
+                        P1Count++;
+                    }
+                    if (grid[x, i] == 2)
+                    {
+                        P2Count++;
+                    }
+
+                    //if count equals 3 display the winning player
+                    if (P1Count == 3)
+                    {
+                        MessageBox.Show("Player 1 Wins!");
+                        break;
+                    }
+                    if (P2Count == 3)
+                    {
+                        MessageBox.Show("Player 2 wins!");
+                        break;
+                    }
+                }
+            }
+        }
+
+        //checks for 3 matches in each diagonal
+        private void CheckDiagonals()
+        {
+            int P1Count = 0;
+            int P2Count = 0;
+            
+            for (int i = 0; i < 3; i++)
+            {
+                //reset count to 0 after each iteration of the loop
+                P1Count = 0;
+                P2Count = 0;
+
+                //check diagonals for 3 matches
+                for (int x = 0; x < 3; x++)
+                {
+                    if (i == x && grid[x, i] == 1)
+                    {
+                        P1Count++;
+                    }
+                    if (i == x && grid[x, i] == 2)
+                    {
+                        P2Count++;
+                    }
+
+                    //if count equals 3 display the winning player
+                    if (P1Count == 3)
+                    {
+                        MessageBox.Show("Player 1 wins!");
+                        break;
+                    }
+
+                    if (P2Count == 3)
+                    {
+                        MessageBox.Show("Player 2 wins!");
+                        break;
+                    }
+                }
+            }
+        }
+
+        //checks for 3 matches in each diagonal
+        private void CheckRows()
+        {
+            int P1Count = 0;
+            int P2Count = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    if (i == x && grid[x, i] == 1)
+                    {
+                        P1Count++;
+                    }
+
+                    if (i == x && grid[x, i] == 2)
+                    {
+                        P2Count++;
+                    }
+
+                    //if count equals 3 display the winning player
+                    if (P1Count == 3)
+                    {
+                        MessageBox.Show("Player 1 wins!");
+                        break;
+                    }
+                    if (P2Count == 3)
+                    {
+                        MessageBox.Show("Player 2 wins!");
+                    }
+                }
+            }
+        }
+    }
+}
